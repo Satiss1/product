42f01919840fbdf2df3a45dee995d05b60a3c6ad
Satiss1 <shresthasatish133@gmail.com>
2024-05-17 16:52:27 -0700

    folder moved

---
+++ SpringBootTraining/src/main/java/com/student/name/Grade10.java
@@ -0,0 +1,51 @@
+package com.student.name;
+
+public class Grade10 {
+
+	private String name;
+	private String age;
+	private String address;
+	private String country;
+	private String pinCode;
+	
+	public Grade10(String name, String age, String address, String country, String pinCode) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.country = country;
+		this.pinCode = pinCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+	
+	
+}
---
+++ SpringBootTraining/src/test/java/Spring/Boot/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
